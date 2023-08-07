8348cc1d19795d0289d6e056a77c0fdcda93b74e
Mensch2023 <135425192+Mensch2023@users.noreply.github.com>
2023-08-07 19:16:12 +0000

    1 file modified

--- src/main.js
+++ src/main.js
@@ -3,3 +3,19 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+template: `<div id="qr-code-full-region"></div>`,
+mounted () {
+  const config = {
+    fps: this.fps,
+    qrbox: this.qrbox,
+  };
+  const html5QrcodeScanner = new Html5QrcodeScanner('qr-code-full-region', config);
+  html5QrcodeScanner.render(this.onScanSuccess);
+},
+methods: {
+  onScanSuccess (decodedText, decodedResult) {
+    this.$emit('result', decodedText, decodedResult);
+  }
+}
+});
